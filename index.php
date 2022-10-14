4da66109b835d00c509f711952e364d6ca2e3c61
areyes2019 <reyesabdias@gmail.com>
2022-10-13 20:52:51 -0500

    Revert "Se agrega un parrafo"
    This reverts commit 6fcd5f50d93515345c9115262d71b7f788b0f700.

--- index.php
+++ index.php
@@ -16,8 +16,7 @@
 	<label for="">Escriba un numero</label>
 	<input type="text" placeholder="Escirba el numero">	
 	<button>Enviar</button>
-	<p>Estoes un cambio</p>
-	<p>Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Magnam minus quis a voluptatem, dolores, aliquid delectus corrupti perferendis, quibusdam, voluptas itaque. Adipisci sit molestiae nesciunt non quasi expedita, doloremque molestias?</p>	
+	<p>Estoes un cambio</p>	
 	<p>
 		<?php
 			$edad_minima = 20;
